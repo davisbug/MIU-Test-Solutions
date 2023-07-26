58feb5aeb789c10f1f018d4ac160f3eadbaadab9
Davis Bug <peter.davis873@gmail.com>
2023-07-26 12:15:08 +0300

    Test_2 Dir rename

---
+++ Test_2/PrimeCount.java
@@ -0,0 +1,40 @@
+package Test_2;
+
+/* 
+ * Date: Jul 26, 2023
+ * Created by davisbug
+ * 
+ */
+
+ public class PrimeCount{
+    public static int primeCount(int start, int end){
+        int primeCount = 0, prime;
+
+        if (start < 2 || end < 2) return 0;
+        for (int iNum = start; iNum <= end ; iNum++) {
+            if(iNum == 2) {
+                primeCount++;
+                continue;
+            }
+            prime = 1;
+            for(int jNum = 2; jNum < iNum; jNum++) {
+                if(iNum % jNum == 0) {
+                    prime = 0;
+                    break;
+                }
+            }
+            if(prime == 1) 
+              primeCount++ ;
+        }
+    
+        return primeCount;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(primeCount(10, 30));
+        System.out.println(primeCount(11, 29));
+        System.out.println(primeCount(20, 22));
+        System.out.println(primeCount(6, 2));
+    }
+ }
