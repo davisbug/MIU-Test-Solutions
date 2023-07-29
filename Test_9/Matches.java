976a57a9badb6cd78b04c51d8e3699dad47b9499
Davis Bug <peter.davis873@gmail.com>
2023-07-29 12:38:05 +0300

    Test_9 : Question 1 to be done

---
+++ Test_9/Matches.java
@@ -0,0 +1,14 @@
+package Test_9;
+/* 
+ * Date: Jul 29, 2023
+ * Created by davisbug
+ * 
+ */
+public class Matches {
+    public static int matches(int[ ] a, int[ ] p){
+
+        //TO BE DONE
+        return 0;
+    }
+    
+}
